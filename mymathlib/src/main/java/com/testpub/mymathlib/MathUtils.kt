5dcdba29b8dfc7ff5e680bd1da112f658ff2b184
pavel-patrusov <pavel@armonic.com>
2024-03-24 15:58:06 +0200

    Init commit

---
+++ mymathlib/src/main/java/com/testpub/mymathlib/MathUtils.kt
@@ -0,0 +1,10 @@
+package com.testpub.mymathlib
+
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+data class Point(val x: Double, val y: Double)
+
+fun Point.distanceToPoint(point: Point): Double {
+    return sqrt((this.x - point.x).pow(2) + (this.y - point.y).pow(2))
+}
